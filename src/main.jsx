77573cba4a10e5b8deca8ab83328aef0690d34f6
Kehinde Yusuf <mubaraq>
2024-08-12 23:53:40 +0100

    Added service worker

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import * as serviceWorkerRegistration from './serviceWorkerRegistration'
+// import './serviceWorker'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -9,4 +9,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   </React.StrictMode>,
 )
 
-serviceWorkerRegistration.unregister();
+
--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,54 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vitejs.dev/config/
+//https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    ValidityStateitePWA({ registerType: 'autoUpdate' })
+    VitePWA(
+      { 
+        registerType: 'autoUpdate',
+        manifest: {
+          name: 'Dictionary App',
+          short_name: 'Dictionary App',
+          description: 'My Awesome App description',
+          theme_color: '#ffffff',
+          icons: [
+            {
+              "src": "/android-chrome-192x192.png",
+              "sizes": "192x192",
+              "type": "image/png"
+            },
+            {
+              "src": "/android-chrome-512x512.png",
+              "sizes": "512x512",
+              "type": "image/png"
+            }
+          ]
+        }
+      })
   ],
-  base: './',
+  // base: './',
 })
+
+// {
+//   "name": "Dictionary App",
+//   "short_name": "Dictionary App",
+//   "theme_color": "#ffffff",
+//   "background_color": "#ffffff",
+//   "display": "standalone",
+//   "scope": "/",
+//   "start_url": "/",
+//   "icons": [
+//     {
+//       "src": "/android-chrome-192x192.png",
+//       "sizes": "192x192",
+//       "type": "image/png"
+//     },
+//     {
+//       "src": "/android-chrome-512x512.png",
+//       "sizes": "512x512",
+//       "type": "image/png"
+//     }
+//   ]
+// }
